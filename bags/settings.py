f10481d5bbea0fbc74b4c42060a7edae00b406ea
mohamed elganainy <78774697+m7mdGNo@users.noreply.github.com>
2022-04-11 06:07:11 +0200

    Update settings.py

--- bags/settings.py
+++ bags/settings.py
@@ -83,15 +83,9 @@ WSGI_APPLICATION = 'bags.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'new2', 
-        'USER': 'postgres', 
-        'PASSWORD': '2591',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
-    }
+    'default': env.db()
 }
+
 
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
